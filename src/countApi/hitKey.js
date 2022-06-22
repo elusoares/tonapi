7e4338bc31d5e8d17c992380f6434dff1dd540cc
Elusai Soares <elusaisoares@gmail.com>
2022-06-22 14:06:34 -0300

    feat(countApi): add lambda function hitKeyHandler This function increases the counter value on CountApi by 1

---
+++ src/countApi/hitKey.js
@@ -0,0 +1,26 @@
+const axios = require('axios').default;
+
+exports.hitKeyHandler = async (event) => {
+    const URL = process.env.COUNT_API_URL;
+    const ENDPOINT = process.env.COUNT_API_HIT_ENDPOINT;
+    const NAMESPACE = process.env.COUNT_API_NAMESPACE;
+    const KEY = process.env.COUNT_API_KEY;
+    
+    try {
+        let axiosResponse = await axios.get(`${URL}${ENDPOINT}${NAMESPACE}${KEY}`);
+        let response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                counterNewValue: axiosResponse.data?.value
+            })
+        };
+
+        return response;
+    } catch (error) {
+        console.log(error.message);
+        let response = {
+            'statusCode': 502,
+            'body': JSON.stringify(error.message)
+        };
+    }
+}
