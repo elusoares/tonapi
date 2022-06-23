80a6b07e5abdbd8293244900762542f230f4f102
Elusai Soares <elusaisoares@gmail.com>
2022-06-23 17:19:23 -0300

    fix(users): fix status code Change status code from 502 to 400 response to getUserByID request error

--- src/users/getUserByID.js
+++ src/users/getUserByID.js
@@ -6,37 +6,26 @@ exports.getUserByIDHandler = async (event) => {
     const userID = event.pathParameters.id;
     const tableName = process.env.USERS;
 
-    if(userID) {
-        let params = {
-            TableName: tableName,
-            Key: {
-                'user_id': {S: userID},
-            },
-            ProjectionExpression: 'user_name, user_email, user_created_at',
-        };
+    let params = {
+        TableName: tableName,
+        Key: {
+            'user_id': {S: userID},
+        },
+        ProjectionExpression: 'user_name, user_email, user_created_at',
+    };
 
-        try {
-            const data = await ddbClient.send(new dynamodb.GetItemCommand(params));
-            response = {
-                'statusCode': 200,
-                'body': JSON.stringify(data.Item)
-            };
-            return response;
-        } catch (error) {
-            response = {
-                'statusCode': 502,
-                'body': error
-            };
-            return response;
-        }
-    } else {
+    try {
+        const data = await ddbClient.send(new dynamodb.GetItemCommand(params));
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify(data.Item)
+        };
+        return response;
+    } catch (error) {
         response = {
             'statusCode': 400,
-            'body': JSON.stringify({
-                message: 'You must provide a user ID'
-            })
+            'body': error
         };
         return response;
     }
-
 }
